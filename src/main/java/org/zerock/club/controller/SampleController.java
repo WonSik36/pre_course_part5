0d8b75e3f6f84227e76d9a58699e3497c867bfb8
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-01-22 22:44:20 +0900

    Change Authorization Logic by changing @Configuraion to @PreAuthorize

--- src/main/java/org/zerock/club/controller/SampleController.java
+++ src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,32 +1,45 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.club.security.dto.ClubAuthMemeberDto;
+import org.zerock.club.security.dto.ClubAuthMemberDto;
 
 @Controller
 @Log4j2
 @RequestMapping("/sample")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll() {
         log.info("exAll..........");
     }
 
     @GetMapping("/member")
-    public void exMember(@AuthenticationPrincipal ClubAuthMemeberDto clubAuthMember) {
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDto clubAuthMember) {
         log.info("exMember..........");
 
         log.info("------------------------");
         log.info(clubAuthMember);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin() {
         log.info("exAdmin..........");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq \"user95@aaa.com\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDto clubAuthMember){
+
+        log.info("exMemberOnly.............");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 }
